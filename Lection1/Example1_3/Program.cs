d68d16ef6a81b927c9ba3abf6d3a7e7a62aa75a4
alesia <albenderina@mail.ru>
2023-01-15 20:54:14 +0300

    Добавили рандомные числа

--- Lection1/Example1_3/Program.cs
+++ Lection1/Example1_3/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Написать программу сложения двух чисел!
-int num1 = 3;
-int num2 = 5;
+int num1 = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
 Console.Write(num1+num2);
