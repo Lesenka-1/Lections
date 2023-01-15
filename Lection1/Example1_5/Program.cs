b26c6d31b32497b75296225e8540437b35300de0
alesia <albenderina@mail.ru>
2023-01-15 21:18:52 +0300

    Особое приветствие

---
+++ Lection1/Example1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу с особенным приветствием
+ Console.WriteLine("Введите ваше имя ");
+ string username  = Console.ReadLine();
+ if (username.ToLower() == "маша")
+ {
+    Console.WriteLine("Это же Маша!!!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.Write(username);
+ }
